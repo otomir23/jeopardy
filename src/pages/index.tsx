7883529a3d40e26399bf414bc60ede6eab1e9616
Damir Modyarov <otomir@yandex.ru>
2022-12-25 13:54:06 +0300

    Implement quiz fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,28 @@
 import {Layout, TeamRegistration} from "../components";
-import {useState} from "react";
-import {Team} from "../types";
+import {useEffect, useState} from "react";
+import {Quiz, Team} from "../types";
 
 export default function Home() {
     const [teams, setTeams] = useState<Team[]>([])
+    const [quiz, setQuiz] = useState<Quiz | null>(null)
+
+    useEffect(() => {
+        fetch('/api/quiz').then(r => r.json()).then(q => setQuiz(q))
+    }, [])
+
+    if (!quiz) {
+        return (
+            <Layout>
+                Loading
+            </Layout>
+        )
+    }
 
     if (teams.length === 0) {
         return (
-            <TeamRegistration
-                onRegister={teams => setTeams(teams)}
-            />
+            <Layout>
+                <TeamRegistration onRegister={teams => setTeams(teams)}/>
+            </Layout>
         )
     }
 
@@ -17,6 +30,7 @@ export default function Home() {
         <Layout>
             <p className="underline">Своя игра.</p>
             {teams.map(t => <p key={t.name}>{t.name}</p>)}
+            {quiz.map(c => <p key={c.name}>{c.name}</p>)}
         </Layout>
     )
 }
